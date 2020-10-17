7cbc0a5e9eedab4fb308da5de040d08a918f5011
Ali Murtaza <58934980+Ali-Murtaza143@users.noreply.github.com>
2020-10-17 03:29:41 -0700

    using for Loop (hacktoberfest)
    #enhancement #hacktoberfest

--- Hello world/Helloworld.java
+++ Hello world/Helloworld.java
@@ -4,4 +4,13 @@ public class Helloworld {
       System.out.println("Hello World");
     }
   }
-  
+                       
+  //print Hello World by using for loop                     
+  public class Helloworld1{
+    public static void main(String[] args) {
+for(int i=0;i<10;i++){ System.out.println("Hello World");
+                     } 
+                       
+                       
+                        }
+    
